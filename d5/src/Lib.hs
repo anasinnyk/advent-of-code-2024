beb5636ee42feae3889a3036032e167590ba842e
Andrii Nasinnyk <anasinnyk@macpaw.com>
2024-12-26 15:12:22 +0200

    d5p2

--- d5/src/Lib.hs
+++ d5/src/Lib.hs
@@ -36,15 +36,31 @@ solve s = sum $ map (\u -> u !! (length u `div` 2)) correctUpdates
     rules = rulesMap rs
     correctUpdates = filter (and . (checkKeys rules)) $ updates
 
+-- | solve'
+-- >>> solve' testData
+-- 123
 solve' :: String -> Int
-solve' = undefined
+solve' s = sum $ map (\u -> u !! (length u `div` 2)) onlyIncorrect
+  where
+    (rs, updates) = parse s
+    rules = rulesMap rs
+    updatesCorection = map (correctUpdates rules) updates
+    onlyIncorrect = map fst $ filter (uncurry (/=)) $ zip updatesCorection updates
 
 rulesMap :: [(Int, Int)] -> Map Int [Int]
 rulesMap = foldl (\m (k,v) -> M.alter (flip (<|>) (Just [v]) . fmap (v :)) k m) M.empty
 
 checkKeys :: M.Map Int [Int] -> [Int] -> [Bool]
 checkKeys _ [] = []
-checkKeys m (x:xs) = maybe (length xs == 0) (\ys -> and (map (\x' -> x' `elem` ys) xs)) (M.lookup x m) : checkKeys m xs
+checkKeys m [x] = [True]
+checkKeys m (x:xs) = maybe False (\ys -> and (map (\x' -> x' `elem` ys) xs)) (M.lookup x m) : checkKeys m xs
+
+correctUpdates :: M.Map Int [Int] -> [Int] -> [Int]
+correctUpdates _ [] = []
+correctUpdates m [x] = [x]
+correctUpdates m (x:xs) = case M.lookup x m of
+  Just ys -> if and (map (`elem` ys) xs) then x: correctUpdates m xs else correctUpdates m (xs ++ [x])
+  Nothing -> correctUpdates m (xs ++ [x])
 
 parse :: String -> ([(Int, Int)], [[Int]])
 parse s = (rules, updates)
